2b1d73545945b0e2472bebe9b8cdcbe2708e2627
PedrohsLeite01 <pedrohslbr@gmail.com>
2025-03-25 10:16:24 -0300

    Initial commit

---
+++ reveal.js
@@ -0,0 +1,45 @@
+window.revelar = ScrollReveal()
+
+/*--------HERRO----*/
+
+revelar.reveal('.hero-1', {
+    duration: 1000, distance: '90px', origin:'bottom'
+})
+
+
+/*---------   VANTAGENS         -----*/
+revelar.reveal('.item-1', {
+    duration: 1000, distance: '90px', origin:'top', delay: 200
+})
+revelar.reveal('.item-2', {
+    duration: 1000, distance: '90px', origin:'top', delay: 300
+})
+revelar.reveal('.item-3', {
+    duration: 1000, distance: '90px', origin:'top', delay: 400
+})
+
+/*-------CONTATO-----*/
+revelar.reveal('.ico-1', {
+    duration: 1000, distance: '90px', origin:'top', delay: 200
+})
+revelar.reveal('.ico-2', {
+    duration: 1000, distance: '90px', origin:'top', delay: 300
+})
+revelar.reveal('.ico-3', {
+    duration: 1000, distance: '90px', origin:'top', delay: 400
+})
+revelar.reveal('.ico-4', {
+    duration: 1000, distance: '90px', origin:'top', delay: 500
+})
+revelar.reveal('.conta-1', {
+    duration: 1000, distance: '90px', origin:'left',
+})
+
+/*------HOTEIS------*/
+revelar.reveal('.hos-1', {
+    duration: 1000, distance: '90px', origin:'top',
+})
+revelar.reveal('.hos-2', {
+    duration: 1000, distance: '90px', origin:'bottom',
+})
+
